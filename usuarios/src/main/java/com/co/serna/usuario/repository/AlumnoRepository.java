127ef0349cc9471e214bf134fdc4480e07a3546d
Juan Serna <jsernaq02@gmail.com>
2024-11-01 17:58:15 -0500

    first commit

---
+++ usuarios/src/main/java/com/co/serna/usuario/repository/AlumnoRepository.java
@@ -0,0 +1,8 @@
+package com.co.serna.usuario.repository;
+
+import com.co.serna.usuario.entity.Alumno;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
+
+}
